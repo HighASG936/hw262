cbd78b966a20a69dfc2ca0aad3397f05432d251f
Aurelio Siordia <lz_rock@hotmail.com>
2024-04-16 22:12:48 -0600

    test

--- examples/SensorTemperature/SensorTemperature.ino
+++ examples/SensorTemperature/SensorTemperature.ino
@@ -8,6 +8,7 @@ uint64_t intervalTime = 100;
 
 void setup()
 {
+    Serial.begin(115200);
     HW262.begin();
     HW262.clearDisplay();
 }
@@ -16,12 +17,12 @@ void setup()
 void loop()
 {
     currentTime = millis();
-    uint32_t voltage = HW262.tempSensor.measureCentigrads();
-
+    uint32_t temperature = HW262.tempSensor.measureCentigrads();
+    Serial.println(String(temperature)+" °C");
     while(elapsedTime<intervalTime)
     {   
         elapsedTime = millis()-currentTime;
-        HW262.writeNumber(voltage);
+        HW262.writeNumber(temperature);
     }
     elapsedTime = 0;
 }
