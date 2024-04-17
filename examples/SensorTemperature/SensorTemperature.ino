e70f593d30cdf7957ba204436ffc463a2bdacbde
Aurelio Siordia <lz_rock@hotmail.com>
2024-04-16 22:30:53 -0600

    example OK

--- examples/SensorTemperature/SensorTemperature.ino
+++ examples/SensorTemperature/SensorTemperature.ino
@@ -4,7 +4,7 @@
 uint16_t number = 0;
 uint64_t currentTime;
 uint64_t elapsedTime = 0;
-uint64_t intervalTime = 100;
+uint64_t intervalTime = 800;
 
 void setup()
 {
@@ -17,7 +17,7 @@ void setup()
 void loop()
 {
     currentTime = millis();
-    float temperature = HW262.tempSensor.measureCentigrads()/100;
+    float temperature = HW262.tempSensor.measureCentigrads()/1000;
     Serial.println(String(temperature)+" °C");
     while(elapsedTime<intervalTime)
     {   
